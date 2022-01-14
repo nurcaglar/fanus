6d7ecdceda001b54e70740a05ad7c442cb7387b1
Nur Caglar <nrcglr.b@gmail.com>
2022-01-14 11:23:24 +0300

    create project

---
+++ fanus/fanus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fanus
+//
+//  Created by Nur Caglar on 14.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fanus/fanus/fanusApp.swift
@@ -0,0 +1,17 @@
+//
+//  fanusApp.swift
+//  fanus
+//
+//  Created by Nur Caglar on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct fanusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
